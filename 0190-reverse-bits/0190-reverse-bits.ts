f5ce7bac8cdbc5119897c4a25022b6c09a7c1577
AKHILJOY312 <156924765+AKHILJOY312@users.noreply.github.com>
2026-03-14 09:12:58 +0530

    Time: 47 ms (65.48%), Space: 55.9 MB (68.18%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.ts
+++ 0190-reverse-bits/0190-reverse-bits.ts
@@ -1,10 +1,7 @@
 function reverseBits(n: number): number {
-    let result=0;
-
-for(let i=0;i<32;i++){
-    result =(result<<1)|(n&1);
-    n>>>=1
-}
-
+    let result = 0;
+    for (let i = 0; i < 32; i++) {
+        result = (result << 1) | ((n >> i) & 1);
+    }
     return result;
 };
