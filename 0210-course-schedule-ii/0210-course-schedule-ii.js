bade6604dd90b8b0c6c1535310112d3c0b6cb373
AKHILJOY312 <156924765+AKHILJOY312@users.noreply.github.com>
2026-03-23 10:28:14 +0530

    Time: 11 ms (52%), Space: 62.5 MB (43.26%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -1,46 +1,64 @@
-/**
- * @param {number} numCourses
- * @param {number[][]} prerequisites
- * @return {number[]}
- */
-var findOrder = function (numCourses, prerequisites) {
-    const adjList = {};
-    const indegree = new Array(numCourses).fill(0);
+class Graph {
+    constructor(numCourses) {
+        this.adList = {};
+        this.indegree = new Array(numCourses).fill(0);
 
-    for (let i = 0; i < numCourses; i++) {
-        adjList[i] = [];
-    }
-
-    for (let [course, pre] of prerequisites) {
-        adjList[pre].push(course);
-        indegree[course]++;
-    }
-
-
-    const queue = [];
-    const result = [];
-
-    for (let i = 0; i < numCourses; i++) {
-        if (indegree[i] === 0) {
-            queue.push(i);
+        for (let i = 0; i < numCourses; i++) {
+            this.adList[i] = [];
         }
     }
 
-    while (queue.length) {
-        const curr = queue.shift();
-        result.push(curr);
+    addVertex(v) {
+        if (!this.adList[v]) {
+            this.adList[v] = [];
+        }
+    }
 
-        for (let neighbor of adjList[curr]) {
-            indegree[neighbor]--;
+    addEdge(pre, course) {
+        this.addVertex(pre);
+        this.addVertex(course);
 
-            if (indegree[neighbor] === 0) {
-                queue.push(neighbor);
+        this.adList[pre].push(course);
+        this.indegree[course]++;
+    }
+
+    bfs(numCourses) {
+        const queue = [];
+        const result = [];
+
+        // push indegree 0
+        for (let i = 0; i < numCourses; i++) {
+            if (this.indegree[i] === 0) {
+                queue.push(i);
             }
         }
+
+        while (queue.length) {
+            const curr = queue.shift();
+            result.push(curr);
+
+            for (let neighbor of this.adList[curr]) {
+                this.indegree[neighbor]--;
+
+                if (this.indegree[neighbor] === 0) {
+                    queue.push(neighbor);
+                }
+            }
+        }
+
+        return result.length === numCourses ? result : [];
+    }
+}
+
+
+
+var findOrder = function(numCourses, prerequisites) {
+
+    const graph = new Graph(numCourses);
+
+    for (let [course, pre] of prerequisites) {
+        graph.addEdge(pre, course);
     }
 
-    if (result.length === numCourses) {
-        return result;
-    }
-    return [];
+    return graph.bfs(numCourses);
 };
