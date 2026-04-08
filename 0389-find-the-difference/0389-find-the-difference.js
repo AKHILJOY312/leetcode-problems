47bd8a7fa2a12a9f3e1487a46f954da3010d0a6d
AKHILJOY312 <156924765+AKHILJOY312@users.noreply.github.com>
2026-04-08 08:59:29 +0530

    Time: 9 ms (21.17%), Space: 55 MB (78.24%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.js
+++ 0389-find-the-difference/0389-find-the-difference.js
@@ -6,7 +6,6 @@
 var findTheDifference = function(s, t) {
     let sArr= s.split("").sort();
     let tArr= t.split("").sort();
-    console.log(sArr,tArr)
     for(let i=0;i<tArr.length;i++){
         if(sArr[i] !== tArr[i]){
             return tArr[i];
