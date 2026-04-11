1ac7b3468cc4d6eccd1ee162bcd94437aa53e42e
AKHILJOY312 <156924765+AKHILJOY312@users.noreply.github.com>
2026-04-11 08:53:36 +0530

    Time: 3 ms (17.19%), Space: 57.3 MB (5.2%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function(s, t) {
+    if(s.length==0)return true;
+    let i=0;
+    for(let char of t){
+
+        if(char === s[i]){
+            i++;
+           if (i === s.length) return true;
+        }
+    }
+    return false;
+};
