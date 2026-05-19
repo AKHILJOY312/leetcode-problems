8d2280e943cff98f4b158ba20cc8835c05bbdbb1
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-05-19 10:34:51 +0530

    Time: 2 ms (6.1%), Space: 55.5 MB (13.37%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.js
+++ 0520-detect-capital/0520-detect-capital.js
@@ -3,13 +3,20 @@
  * @return {boolean}
  */
 var detectCapitalUse = function(word) {
-    const isUpperCase=(char)=>char===char.toUpperCase();
-    let countUpper=0;
-    for(let char of word){
-        if(isUpperCase(char)){
+
+    const isUpperCase = (char) => char === char.toUpperCase();
+
+    let countUpper = 0;
+
+    for (let char of word) {
+        if (isUpperCase(char)) {
             countUpper++;
         }
     }
 
-    return (countUpper===word.length || countUpper===0 || (countUpper===1 && word[0]===word[0].toUpperCase()))
+    return (
+        countUpper === word.length ||
+        countUpper === 0 ||
+        (countUpper === 1 && isUpperCase(word[0]))
+    );
 };
