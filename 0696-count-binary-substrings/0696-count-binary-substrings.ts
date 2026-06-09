919bf4a5ccdb78f67d53c23e618429c334e20070
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-09 12:54:48 +0530

    Time: 5 ms (97.37%), Space: 58.9 MB (84.21%) - LeetHub

---
+++ 0696-count-binary-substrings/0696-count-binary-substrings.ts
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countBinarySubstrings = function (s) {
+    let prev = 0;
+    let curr = 1;
+    let result = 0;
+
+    for (let i = 1; i < s.length; i++) {
+        if (s[i] === s[i - 1]) {
+            curr++;
+        } else {
+            result += Math.min(prev, curr);
+            prev = curr;
+            curr = 1;
+        }
+    }
+
+    result += Math.min(prev, curr);
+
+    return result;
+};
