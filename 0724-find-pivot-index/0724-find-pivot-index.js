bceb75661a6ad645cd4e6bf26554fb8d5c42dd4c
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-12 10:54:23 +0530

    Time: 3 ms (48.01%), Space: 57.2 MB (69.22%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -16,13 +16,9 @@ var pivotIndex = function (nums) {
     for (let i = len - 2; i >= 0; i--) {
         sumRight[i] = sumRight[i + 1] + nums[i]
     }
-console.log(sumLeft,sumRight)
+
     for (let i = 0; i < len; i++) {
-        // leftSum = sumLeft[i] - nums[i]
-        // rightSum = sumRight[i] - nums[i]
-        // if (leftSum == rightSum) {
-        //     return i;
-        // }
+     
         if(sumLeft[i]==sumRight[i]){
             return i;
         }
