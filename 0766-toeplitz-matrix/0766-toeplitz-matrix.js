5e8e80bcbb000de294f805c5ef2e784e60460af6
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-15 12:23:14 +0530

    Time: 1 ms (52%), Space: 55.8 MB (60%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var isToeplitzMatrix = function(matrix) {
+    let n= matrix.length;
+    let m= matrix[0].length;
+
+    for(let i=1;i<n;i++){
+        for(let j=1;j<m;j++){
+            if(matrix[i][j] !== matrix[i-1][j-1]){
+                return false;
+            }
+        }
+    }
+    return true;
+};
