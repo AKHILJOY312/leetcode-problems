eab184cf982e6a427c74b5a26152172dd14377eb
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-16 10:30:46 +0530

    Time: 39 ms (83.53%), Space: 53.7 MB (81.18%) - LeetHub

---
+++ 0806-number-of-lines-to-write-string/0806-number-of-lines-to-write-string.js
@@ -0,0 +1,17 @@
+var numberOfLines = function (widths, s) {
+    let lines = 1;
+    let currentWidth = 0;
+
+    for (let i = 0; i < s.length; i++) {
+        const width = widths[s.charCodeAt(i) - 97];
+
+        if (currentWidth + width > 100) {
+            lines++;
+            currentWidth = width;
+        } else {
+            currentWidth += width;
+        }
+    }
+
+    return [lines, currentWidth];
+};
