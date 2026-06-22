18a3b7ba78fc3f084de4ce2d8f37e92707ad561e
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-22 11:19:45 +0530

    Time: 0 ms (100%), Space: 53.9 MB (94.68%) - LeetHub

---
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.ts
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function swapPairs(head: ListNode | null): ListNode | null {
+
+    let dummy: ListNode = new ListNode(0);
+    dummy.next = head;
+    let prev = dummy;
+
+    while (prev.next && prev.next.next) {
+        let first = prev.next;
+        let second = first.next!;
+
+        //swap
+        first.next = second.next;
+        second.next = first;
+        prev.next = second;
+
+        //Move to the next pair
+        prev = first;
+
+    }
+
+    return dummy.next;
+};
