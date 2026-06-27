007a92f3dcb67130449e3c83b43bef73ca8a5ab4
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-06-27 11:17:18 +0530

    Time: 0 ms (100%), Space: 53.8 MB (32.21%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -1,32 +1,33 @@
-var letterCombinations = function (digits) {
-    if (digits.length === 0) return [];
-
-    const phone = {
-        2: "abc",
-        3: "def",
-        4: "ghi",
-        5: "jkl",
-        6: "mno",
-        7: "pqrs",
-        8: "tuv",
-        9: "wxyz"
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+   const phone = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
     };
-
-    const result = [];
-
-    function backtrack(index, current) {
-        if (index === digits.length) {
-            result.push(current);
-            return;
+const res=[];
+    function backTracking(index,current){
+        //base case
+        if(index===digits.length){
+            res.push(current);
+            return
         }
 
-        const letters = phone[digits[index]];
-
-        for (let ch of letters) {
-            backtrack(index + 1, current + ch);
+        //take the letter out 
+        let letters= phone[digits[index]];
+        for(let ch of letters){
+            backTracking(index+1,current+ch);
         }
     }
 
-    backtrack(0, "");
-    return result;
+    backTracking(0,[]);
+    return res;
 };
