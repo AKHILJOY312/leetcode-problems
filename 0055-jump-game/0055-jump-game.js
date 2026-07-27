cbc3e1708842c2ced7268f54e7febf1235b893a2
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-07-27 18:40:18 +0530

    Time: 0 ms (100%), Space: 59 MB (64.09%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,14 +2,12 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function (nums) {
-    let max_reach = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (i > max_reach) return false;
-
-        max_reach = Math.max(max_reach, i + nums[i]);
-        if (max_reach >= nums.length - 1) return true;
+var canJump = function(nums) {
+    let goal= nums.length-1;
+    for(let i=nums.length-2;i>=0;i--){
+        if(i+nums[i]>=goal){
+            goal= i
+        }
     }
-
-    return true;
+    return goal===0;
 };
