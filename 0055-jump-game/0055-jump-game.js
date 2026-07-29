ba4d08c05efbec68456747f57daaf90a0b86b56d
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-07-29 09:12:46 +0530

    Time: 1470 ms (5.02%), Space: 61.5 MB (11.1%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -2,12 +2,26 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var canJump = function(nums) {
-    let goal= nums.length-1;
-    for(let i=nums.length-2;i>=0;i--){
-        if(i+nums[i]>=goal){
-            goal= i
+var canJump = function (nums) {
+    //create ap array with the length of the given array and fill it with false
+    const dp = new Array(nums.length).fill(false);
+
+    //initialize the first element of dp to true
+    dp[0] = true;
+
+    //Create a loop that runs the with the length of the give array
+    for (let i = 0; i < nums.length; i++) {
+        //If the current element is false then contine to the next loop
+        if (!dp[i]) continue;
+
+        //Create a loop for the maximum jump for current index
+        for (let jump = 1; jump <= nums[i]; jump++) {
+            //If the the jump from the currect index is less, than keep the dp array true
+            if (i + jump < nums.length) {
+                dp[jump + i] = true;
+            }
         }
     }
-    return goal===0;
+    //return the last index element in the dp
+    return dp[nums.length - 1]
 };
