725197bf6b82f54c8b7d761070ec913a922cebc0
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-08-11 09:32:02 +0530

    Time: 565 ms (5.01%), Space: 65 MB (5.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -2,33 +2,23 @@
  * @param {number[]} nums
  * @return {number}
  */
-var jump = function (nums) {
-    //create an array with the same length as nums and fill it with -1
-    const dp = new Array(nums.length).fill(-1);
+var jump = function(nums) {
+    
+    let jumps=0;
+    let currentEnd=0;
+    let farthest=0;
 
-    function minJump(index) {
-        //check wheter i have reachde the index or passed the index
-        if (index >= nums.length - 1) {
-            return 0;
-        }
+    for(let i=0;i<nums.length-1;i++){
+        farthest=Math.max(farthest,i+nums[i])
+        console.log("farthest:",farthest)
+         console.log("jumps:",jumps)
+          console.log("currentEnd:",currentEnd)
+           console.log("_________")
 
-        //if the value at dp intex is not -1 then return it
-        if (dp[index] !== -1) {
-            return dp[index];
+        if(i===currentEnd){
+            jumps++;
+            currentEnd= farthest;
         }
-
-        let min = Infinity;
-        //Try all the possible jumps
-        for (let jump = 1; jump <= nums[index]; jump++) {
-            //make the jumps from here to the spefied possbils location
-            const jumps = minJump(index + jump);
-            //keep track on the minumum
-            min = Math.min(jumps, min)
-        }
-        dp[index]=1+min;
-        return dp[index]
     }
-
-    //run the function
-    return minJump(0)
+    return jumps;
 };
