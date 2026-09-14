65b4cac48188fd02adf429cd949a68d4beec0e7a
Akhil Joy <156924765+AKHILJOY312@users.noreply.github.com>
2026-09-14 13:53:29 +0530

    Time: 0 ms (100%), Space: 53.9 MB (49.69%) - LeetHub

--- 0274-h-index/0274-h-index.js
+++ 0274-h-index/0274-h-index.js
@@ -2,17 +2,29 @@
  * @param {number[]} citations
  * @return {number}
  */
-var hIndex = function(citations) {
-    citations.sort((a,b)=>a-b);
+var hIndex = function (citations) {
+    const n = citations.length;
 
-    const lenghtOfPappers=citations.length;
+    const buckets = new Array(n + 1).fill(0);
 
-    for(let i=0;i<lenghtOfPappers;i++){
-        const pappers= lenghtOfPappers-i;
-
-        if(citations[i]>=pappers){
-            return pappers
+    // Build buckets
+    for (const citation of citations) {
+        if (citation >= n) {
+            buckets[n]++;
+        } else {
+            buckets[citation]++;
         }
     }
+
+    let papers = 0;
+
+    for (let h = n; h >= 0; h--) {
+        papers += buckets[h];
+
+        if (papers >= h) {
+            return h;
+        }
+    }
+
     return 0;
 };
